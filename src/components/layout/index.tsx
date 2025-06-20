564a72d807cc694e800c63d2f228da228d4077c2
riyapojage <133016157+riyapojage@users.noreply.github.com>
2025-06-20 19:19:26 +0530

    Initial commit: AdminSynch CRM project

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,21 +2,17 @@ import React from "react";
 
 import { ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd";
 
-import { GitHubBanner } from "./gh-banner";
 import { Header } from "./header";
 
 export const Layout = ({ children }: React.PropsWithChildren) => {
   return (
-    <>
-      <GitHubBanner />
-      <ThemedLayoutV2
-        Header={Header}
-        Title={(titleProps) => {
-          return <ThemedTitleV2 {...titleProps} text="Refine" />;
-        }}
-      >
-        {children}
-      </ThemedLayoutV2>
-    </>
+    <ThemedLayoutV2
+      Header={Header}
+      Title={(titleProps) => {
+        return <ThemedTitleV2 {...titleProps} text="Refine" />;
+      }}
+    >
+      {children}
+    </ThemedLayoutV2>
   );
 };
